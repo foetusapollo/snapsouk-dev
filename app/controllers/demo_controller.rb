14d713a83c69fad02a703183570601552ac39292
foetusapollo <andrew@snapsouk.com>
2012-06-26 00:26:26 -0400

    More playing around with controllers and views, testing embedded ruby stuff like that.

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -6,11 +6,21 @@ class DemoController < ApplicationController
       # Rails 2.0 style
       # render(:template => 'demo/hello') or render(demo/hello)
       # Rails 3.0 -- as long as it's in the same folder, otherwise just tell it the folder to look in
-      render('hello')
+      # render('hello')
+      # how a redirect works. specify :controller in the hash if necessary.
+      # redirect_to(:action => 'other_hello')
   end
     
   def hello
-    render(:text => 'Hello everyone!')
+    # render(:text => 'Hello everyone!')
+    # redirect_to("http://www.lynda.com")
+    @array = [1,2,3,4,5]
+    @id = params[:id].to_i
+    @page = params[:page].to_i
   end
     
+  def other_hello
+    render(:text => 'Hello everyone!')
+  end
+  
 end
