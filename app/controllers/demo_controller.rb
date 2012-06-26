1ddb61b70dad607dc51976db787202e597838032
foetusapollo <andrew@snapsouk.com>
2012-06-25 23:17:40 -0400

    Playing around with the demo controller

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,4 +1,16 @@
 class DemoController < ApplicationController
+  
   def index
+      # Rails 1.0 Style
+      # render(:action => 'hello')
+      # Rails 2.0 style
+      # render(:template => 'demo/hello') or render(demo/hello)
+      # Rails 3.0 -- as long as it's in the same folder, otherwise just tell it the folder to look in
+      render('hello')
   end
+    
+  def hello
+    render(:text => 'Hello everyone!')
+  end
+    
 end
