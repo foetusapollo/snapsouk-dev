fee534d4d0ddeb56a9eb5bd19ea7df3a193efa5c
foetusapollo <andrew@snapsouk.com>
2012-06-26 17:57:07 -0400

    created a named scope.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,1 +1,9 @@
+class AdminUser < ActiveRecord::Base
+  attr_accessible :first_name, :last_name, :username, :email, :style_id
+  
+  scope :style_id, where(:style_id => 1)
+  
+  # To configure a different table name with a legacy DB
+  # set_table_name("admin_users")
+  
 end
